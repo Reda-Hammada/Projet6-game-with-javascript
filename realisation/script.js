8200f40f56ed1b0c4dfbe0209a8fc602206c1013
redahm <hammada.reda.dev@gmail.com>
2021-11-30 12:37:05 +0100

    varibales and treatment of program added

--- realisation/script.js
+++ realisation/script.js
@@ -1,2 +1,37 @@
+//variable declarations:
+var btn
 var randoMnumber;
 var output;
+var guessedNumber;
+var attempts;
+var lostMessage;
+
+// entrer: saiser 
+
+btn = document.getElementById('btn');
+lostMessage ="you have lost after";
+guessedNumber = document.getElementById('output').value;
+attempts = 10;
+randoMnumber = Math.floor(Math.random() * 100);
+
+btn.addEventListener('click',  function(){
+
+    if( randoMnumber == guessedNumber) {
+
+
+        output = this.innerHTML = 'correct';
+    }
+
+    else if (randoMnumber > guessedNumber) {
+
+        output = this.innerHTML = "le nombre est plus grand que" + r ;
+    }
+
+    else {
+
+        output = this.innerHTML = "le nombre est moin que"
+    }
+
+
+});
+
